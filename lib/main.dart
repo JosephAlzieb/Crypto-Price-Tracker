f2935b30de8cbd8ed7199bc2c4474cfc8fab02e2
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-01-18 22:44:53 +0100

    edit sdk

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:crypto_price_tracker/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,16 +6,17 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Crypto Price Tracker',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: null,
+      home: HomePage(),
     );
   }
 }
