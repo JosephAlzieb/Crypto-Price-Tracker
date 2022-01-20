494d7ea6c6c4de7a061bff8d14278fa52229c84c
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-01-20 01:28:00 +0100

    create a Coin-model to get the data from crypto-api

---
+++ lib/coin.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Coin {
+  String name;
+  String symbol;
+  String imageUrl;
+  double price;
+  double changePrecentage;
+
+  Coin({
+    @required this.name,
+    @required this.symbol,
+    @required this.imageUrl,
+    @required this.price,
+    @required this.changePrecentage,
+  });
+
+  factory Coin.fromJson(Map<String, dynamic> json) {
+    return Coin(
+        name: json['name'],
+        symbol: json['symbol'],
+        imageUrl: json['image'],
+        price: json['current_price'],
+        changePrecentage: json['price_change_percentage_24h']);
+  }
+
+  List<Coin> coinList = [];
+}
