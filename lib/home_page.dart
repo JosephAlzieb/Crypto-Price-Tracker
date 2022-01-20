bb66c06f5a78e32381f3437313dca99701258444
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-01-20 02:42:36 +0100

    add a listview.builder to show the complete coin list

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,18 +13,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  Future<List<Coin>> fetchCoin () async{
-
-    final response = await http.get(Uri.parse('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'));
-
+  Future<List<Coin>> fetchCoin() async {
     coinList = [];
-    if(response.statusCode == 200){
+
+    final response = await http.get(Uri.parse(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'));
+
+    if (response.statusCode == 200) {
       List<dynamic> values = [];
       values = json.decode(response.body);
-      if(values.isNotEmpty){
-        for (int i = 0; i < values.length; i++){
-          if(values[i] != null){
+      if (values.length > 0) {
+        for (int i = 0; i < values.length; i++) {
+          if (values[i] != null) {
             Map<String, dynamic> map = values[i];
             coinList.add(Coin.fromJson(map));
           }
@@ -34,8 +34,7 @@ class _HomePageState extends State<HomePage> {
         });
       }
       return coinList;
-    }
-    else {
+    } else {
       throw Exception("faild to load coins");
     }
   }
@@ -60,45 +59,17 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      body: ListView(
-        children: [
-          CoinCard(name: "Bitcoin",
-              symbol: "btc",
-              imageUrl: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
-              price: 41482,
-              changePrecentage: 20),
-          CoinCard(name: "Ethereum",
-              symbol: "eth",
-              imageUrl: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880',
-              price: 3191.78,
-              changePrecentage: -2.6),
-          CoinCard(name: "Solana",
-              symbol: "sol",
-              imageUrl: 'https://assets.coingecko.com/coins/images/4128/large/solana.png?1640133422',
-              price: 137.3,
-              changePrecentage: -7),
-          CoinCard(name: "Polkadot",
-              symbol: "dot",
-              imageUrl: 'https://assets.coingecko.com/coins/images/12171/large/polkadot.png?1639712644',
-              price: 25.36,
-              changePrecentage: 5.2),
-          CoinCard(name: "Chainlink",
-              symbol: "link",
-              imageUrl: 'https://assets.coingecko.com/coins/images/877/large/chainlink-new-logo.png?1547034700',
-              price: 23.27,
-              changePrecentage: -22.39),
-          CoinCard(name: "Decentraland",
-              symbol: "mana",
-              imageUrl: 'https://assets.coingecko.com/coins/images/878/large/decentraland-mana.png?1550108745',
-              price:  2.87,
-              changePrecentage: -2.9),
-          CoinCard(name: "Litecoin",
-              symbol: "ltc",
-              imageUrl: 'https://assets.coingecko.com/coins/images/2/large/litecoin.png?1547033580',
-              price:   142.81,
-              changePrecentage: 0.32),
-
-        ],
+      body: ListView.builder(
+        itemCount: coinList.length,
+        itemBuilder: (context, index) {
+          return CoinCard(
+            name: coinList[index].name,
+            symbol: coinList[index].symbol,
+            imageUrl: coinList[index].imageUrl,
+            price: coinList[index].price.toDouble(),
+            changePrecentage: coinList[index].changePrecentage.toDouble(),
+          );
+        },
       ),
     );
   }
